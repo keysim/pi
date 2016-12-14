1955c5a91ec0a659f2634db25ad8086aa78375ef
menard_s <simon.menard@epitech.eu>
2016-12-14 13:40:03 +0800

    test

--- test.js
+++ test.js
@@ -1,8 +1,19 @@
+var http = require('http');
 var exec = require('child_process').exec;
-exec('gpio write 1 0', function(error, stdout, stderr) {
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
-    if (error !== null) {
-        console.log('exec error: ' + error);
-    }
-});
+
+var visits = 0;
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    visits = !visits;
+    var msg = 'Light: ' + visits ? "true":"false";
+    res.end(msg + '\n');
+    console.log(msg);
+    exec('gpio write 1 ' + String(visits), function(error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+            console.log('exec error: ' + error);
+        }
+    });
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
