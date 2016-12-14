79264aacd3cdae18ee0d5f3e6936be26e92e4621
menard_s <simon.menard@epitech.eu>
2016-12-14 13:54:56 +0800

    test

--- test.js
+++ test.js
@@ -2,13 +2,17 @@ var http = require('http');
 var exec = require('child_process').exec;
 
 var visits = 0;
+var light = false;
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    visits = !visits;
-    var msg = 'Light: ' + (visits ? "true":"false");
+
+    visits += 1;
+    if(visits % 2 == 1)
+        light = !light;
+    var msg = 'Light: ' + (light ? "true":"false");
     res.end(msg + '\n');
     console.log(msg);
-    exec('gpio write 1 ' + (visits ? "1" : "0"), function(error, stdout, stderr) {
+    exec('gpio write 1 ' + (light ? "0" : "1"), function(error, stdout, stderr) {
         //console.log('stdout: ' + stdout);
         //console.log('stderr: ' + stderr);
         if (error !== null)
