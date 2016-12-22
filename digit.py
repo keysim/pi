4320e62b8fc24c04eeb94248a5e62c182fa91092
menard_s <simon.menard@epitech.eu>
2016-12-22 17:59:50 +0800

    test

--- digit.py
+++ digit.py
@@ -4,12 +4,14 @@ import serial
 import netifaces as ni
 
 ser = serial.Serial('/dev/ttyUSB0', 9600)
-
+time.sleep(1)
 ni.ifaddresses('eth0')
 ip = ni.ifaddresses('eth0')[2][0]['addr']
 print(ip)  # should print "192.168.100.37"
 
 while 1 :
   for c in ip:
-    time.sleep(1)
     ser.write(c.encode('ascii'))
+    time.sleep(.7)
+  ser.write(".".encode('ascii'))
+  time.sleep(2)
