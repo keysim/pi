04bd6070a8577c6979178cae4b7cd9896d4d6e33
menard_s <simon.menard@epitech.eu>
2016-12-22 17:20:37 +0800

    test

--- digit.py
+++ digit.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
 import time
 import serial
+import netifaces as ni
 
 ser = serial.Serial('/dev/ttyUSB0', 9600)
 
-addr = "$(hostname -I)"
+ni.ifaddresses('eth0')
+ip = ni.ifaddresses('eth0')[2][0]['addr']
+print(ip)  # should print "192.168.100.37"
 
 print(addr)
 
